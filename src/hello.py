72b98cb4f3ec363a6cb9e44091bd6681d2fcf687
jipg <jorgeivan.posada@gmail.com>
2022-12-18 15:02:24 -0500

    fix(msg): Change universe msg

--- src/hello.py
+++ src/hello.py
@@ -5,4 +5,4 @@
 import sys
 
 print(f"\n\n ### \n\nHello this a deploy to {sys.argv[1]}.\n\n ### \n\n")
-print("\n\n ### \n\nBye cruel world and universe too-RE.\n\n ### \n\n")
+print("\n\n ### \n\nBye cruel world and universe too-Re.\n\n ### \n\n")
