6aee7cf0fcb8777bcc2b5c2ec940dc87a7ee45b9
jipg <jorgeivan.posada@gmail.com>
2022-12-18 06:25:33 -0500

    fix(msg): Test bump with the message

--- src/hello.py
+++ src/hello.py
@@ -5,4 +5,4 @@
 import sys
 
 print(f"\n\n ### \n\nHello this a deploy to {sys.argv[1]}.\n\n ### \n\n")
-print("\n\n ### \n\nBye cruel world and universe too.\n\n ### \n\n")
+print("\n\n ### \n\nBye cruel world and universe.\n\n ### \n\n")
